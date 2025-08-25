8eacf80aad4f34b6e463de926cde2c8fc4ebedba
Turbobent <kasp728h@edu.mercantec.dk>
2025-08-25 11:46:42 +0200

    main.c and cords

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <windows.h>
+
+COORD coord = {0, 0};
+//snake game
+int main (){
+
+}
+
+void gotoxy(int x,int y){
+    coord.X = x; coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+}
