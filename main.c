f0ca1332da002b1a3d0eceb9fa0e0719a3246261
Turbobent <kasp728h@edu.mercantec.dk>
2025-08-27 14:42:02 +0200

    commented bend out

--- main.c
+++ main.c
@@ -64,71 +64,71 @@ void down(){
     //for(i = 0; i<=(head.y-)){}
 }
 
-void Bend()
-{
-    int i,j,diff;
-    for(i=bend_no; i>=0&&len<length; i--)
-    {
-        if(bend[i].x==bend[i-1].x)
-        {
-            diff=bend[i].y-bend[i-1].y;
-            if(diff<0)
-                for(j=1; j<=(-diff); j++)
-                {
-                    body[len].x=bend[i].x;
-                    body[len].y=bend[i].y+j;
-                    GotoXY(body[len].x,body[len].y);
-                    printf("*");
-                    len++;
-                    if(len==length)
-                        break;
-                }
-            else if(diff>0)
-                for(j=1; j<=diff; j++)
-                {
-                    /*GotoXY(bend[i].x,(bend[i].y-j));
-                    printf("*");*/
-                    body[len].x=bend[i].x;
-                    body[len].y=bend[i].y-j;
-                    GotoXY(body[len].x,body[len].y);
-                    printf("*");
-                    len++;
-                    if(len==length)
-                        break;
-                }
-        }
-        else if(bend[i].y==bend[i-1].y)
-        {
-            diff=bend[i].x-bend[i-1].x;
-            if(diff<0)
-                for(j=1; j<=(-diff)&&len<length; j++)
-                {
-                    /*GotoXY((bend[i].x+j),bend[i].y);
-                    printf("*");*/
-                    body[len].x=bend[i].x+j;
-                    body[len].y=bend[i].y;
-                    GotoXY(body[len].x,body[len].y);
-                    printf("*");
-                    len++;
-                    if(len==length)
-                        break;
-                }
-            else if(diff>0)
-                for(j=1; j<=diff&&len<length; j++)
-                {
-                    /*GotoXY((bend[i].x-j),bend[i].y);
-                    printf("*");*/
-                    body[len].x=bend[i].x-j;
-                    body[len].y=bend[i].y;
-                    GotoXY(body[len].x,body[len].y);
-                    printf("*");
-                    len++;
-                    if(len==length)
-                        break;
-                }
-        }
-    }
-}
+// {void Bend()
+// {
+//     int i,j,diff;
+//     for(i=bend_no; i>=0&&len<length; i--)
+//     {
+//         if(bend[i].x==bend[i-1].x)
+//         {
+//             diff=bend[i].y-bend[i-1].y;
+//             if(diff<0)
+//                 for(j=1; j<=(-diff); j++)
+//                 {
+//                     body[len].x=bend[i].x;
+//                     body[len].y=bend[i].y+j;
+//                     GotoXY(body[len].x,body[len].y);
+//                     printf("*");
+//                     len++;
+//                     if(len==length)
+//                         break;
+//                 }
+//             else if(diff>0)
+//                 for(j=1; j<=diff; j++)
+//                 {
+//                     /*GotoXY(bend[i].x,(bend[i].y-j));
+//                     printf("*");*/
+//                     body[len].x=bend[i].x;
+//                     body[len].y=bend[i].y-j;
+//                     GotoXY(body[len].x,body[len].y);
+//                     printf("*");
+//                     len++;
+//                     if(len==length)
+//                         break;
+//                 }
+//         }
+//         else if(bend[i].y==bend[i-1].y)
+//         {
+//             diff=bend[i].x-bend[i-1].x;
+//             if(diff<0)
+//                 for(j=1; j<=(-diff)&&len<length; j++)
+//                 {
+//                     /*GotoXY((bend[i].x+j),bend[i].y);
+//                     printf("*");*/
+//                     body[len].x=bend[i].x+j;
+//                     body[len].y=bend[i].y;
+//                     GotoXY(body[len].x,body[len].y);
+//                     printf("*");
+//                     len++;
+//                     if(len==length)
+//                         break;
+//                 }
+//             else if(diff>0)
+//                 for(j=1; j<=diff&&len<length; j++)
+//                 {
+//                     /*GotoXY((bend[i].x-j),bend[i].y);
+//                     printf("*");*/
+//                     body[len].x=bend[i].x-j;
+//                     body[len].y=bend[i].y;
+//                     GotoXY(body[len].x,body[len].y);
+//                     printf("*");
+//                     len++;
+//                     if(len==length)
+//                         break;
+//                 }
+//         }
+//     }
+// }}
 
 void Move(){
 
